2156e6d898f8651899f95beb5ef5e60977173e3e
smohammad <smohammad@orion180.com>
2022-12-11 18:47:21 +0530

    Setting up angular jwt and modules import

--- VendingMachine/src/app/app.module.ts
+++ VendingMachine/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JwtModule } from "@auth0/angular-jwt";
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +15,10 @@ import { AddProductsComponent } from './seller/add-products/add-products.compone
 import { BuyComponent } from './buyer/buy/buy.component';
 import { ViewComponent } from './buyer/view/view.component';
 import { DepositComponent } from './buyer/deposit/deposit.component';
+
+export function tokenGetter() {
+  return localStorage.getItem("access_token");
+}
 
 @NgModule({
   declarations: [
@@ -25,7 +33,19 @@ import { DepositComponent } from './buyer/deposit/deposit.component';
     DepositComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, 
+    HttpClientModule, 
+    AppRoutingModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["example.com"],
+        disallowedRoutes: [],
+      },
+    }),
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
