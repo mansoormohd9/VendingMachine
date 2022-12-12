c0ba43e735e2192bb30e4fdf28ca38e46e871f49
smohammad <smohammad@orion180.com>
2022-12-12 13:33:20 +0530

    seller products view

--- VendingMachine/src/app/seller/view-products/view-products.component.ts
+++ VendingMachine/src/app/seller/view-products/view-products.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ProductDto } from '../models/Seller';
+import { SellerService } from '../seller.service';
 
 @Component({
   selector: 'app-view-products',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-products.component.css']
 })
 export class ViewProductsComponent {
+  products!: Array<ProductDto>;
+  constructor(private sellerService: SellerService, private toastr: ToastrService){ }
 
+  ngOnInit(): void {
+    this.fetchDeposits();
+  }
+
+  fetchDeposits() {
+    this.sellerService.viewProducts()
+    .subscribe({
+      next: data => {
+        this.products = data;
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    })
+  }
+
+  deleteProduct = (e:Event, id: number) => {
+    e.preventDefault();
+    this.sellerService.deleteProduct(id).subscribe({
+      next: data => {
+        this.products = this.products?.filter(x => x.id != id);
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    })
+  }
 }
