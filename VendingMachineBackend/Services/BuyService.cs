ec8d0d3db217f7a46ba899358aaaefa42762457b
smohammad <smohammad@orion180.com>
2022-12-13 19:04:51 +0530

    Handle product buy with coin change memoization

--- VendingMachineBackend/Services/BuyService.cs
+++ VendingMachineBackend/Services/BuyService.cs
@@ -73,6 +73,11 @@ namespace VendingMachineBackend.Services
             var userDeposits = _userDepositRepository.Find(x => x.UserId == au.Id).ToDictionary(x => x.Deposit.Amount, v => v.Quantity);
             var depoistAmountMap = _depositRepository.Find(x => userDeposits.ContainsKey(x.Id)).ToDictionary(x => x.Id, x => x.Amount);
 
+            var coinsAvailable = depoistAmountMap.Values.ToArray();
+            var limits = coinsAvailable.Select(x => userDeposits[x]).ToArray();
+
+            var coinChanges = CoinChangeWithMemoization(coinsAvailable, limits, totalAmount);
+
             var amountsAvailable = userDeposits.Keys.OrderByDescending(x => x).ToHashSet();
 
             //TODO fix below logic to handle all cases
@@ -100,6 +105,91 @@ namespace VendingMachineBackend.Services
             }
 
             return new Result<Dictionary<decimal, int>>(true, string.Empty, userDeposits);
+        }
+
+        public (int minCount, int[] arr) CoinChangeWithMemoization(int[] coins, int[] limits, int amount)
+        {
+            int[,] t = new int[coins.Length, amount + 1];
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = 0; j < amount + 1; j++)
+                {
+                    t[i, j] = -1;
+                }
+            }
+
+            var result = SolveCoinChange(coins, limits, coins.Length - 1, t, amount);
+
+            if (result.minCount == int.MaxValue - 1)
+            {
+                return (-1, result.arr);
+            }
+
+            return (-1, result.arr);
+        }
+
+
+        public (int minCount, int[] arr) SolveCoinChange(int[] coins, int[] limits, int n, int[,] t, int amount)
+        {
+            if (n < 0)
+            {
+                return (-1, limits);
+            }
+
+            if (amount == 0)
+            {
+                return (0, limits);
+            }
+
+            if (n == 0 && amount > 0)
+            {
+                var requiredQuantity = amount / coins[n];
+                if ((amount % coins[n] == 0) && limits[n] >= requiredQuantity)
+                {
+                    limits[n] = limits[n] - requiredQuantity;
+                    return (requiredQuantity, limits);
+                }
+                else
+                {
+                    return (int.MaxValue - 1, limits);
+                }
+            }
+
+            if (t[n, amount] != -1)
+            {
+                return (t[n, amount], limits);
+            }
+
+            (int minCount, int[] arr) result;
+            if (limits[n] == 0)
+            {
+                result = SolveCoinChange(coins, limits, n - 1, t, amount);
+                t[n, amount] = result.minCount;
+                return result;
+            }
+
+            if (coins[n] <= amount)
+            {
+                var temp = new int[coins.Length];
+                Array.Copy(limits, temp, coins.Length);
+
+                temp[n]--;
+                var result1 = 1 + SolveCoinChange(coins, temp, n, t, amount - coins[n]).minCount;
+                var result2 = SolveCoinChange(coins, limits, n - 1, t, amount).minCount;
+
+
+                if (result1 < result2)
+                {
+                    Array.Copy(temp, limits, coins.Length);
+                    t[n, amount] = result1;
+                    return (result1, limits);
+                }
+                return (result2, limits);
+            }
+
+            t[n, amount] = SolveCoinChange(coins, limits, n - 1, t, amount).minCount;
+            return (t[n, amount], limits);
         }
     }
 }
